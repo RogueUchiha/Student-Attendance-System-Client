cbd4116be529263a75d2d25e9aff02491c755a18
Colton Cross <82963784+ColeCross@users.noreply.github.com>
2022-04-28 12:27:16 -0400

    Fixed some name issues

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -6,10 +6,8 @@ import {
     BrowserRouter
   } from "react-router-dom";
 import Home from "./Home";
-import ReportGen from "./ReportGen";
-import Backup from "./Backup";
-import Update from "./Update";
-import Upload from "./Upload";
+import ReportGen from "./AdminReportPage";
+import Backup from "./AdminBackupPage";
  
 class Main extends Component {
     render() {
@@ -25,16 +23,12 @@ class Main extends Component {
                         <li><NavLink to="/">Home</NavLink></li>
                         <li><NavLink to="/report">Attendance Reports</NavLink></li>
                         <li><NavLink to="/backup">Backup Database</NavLink></li>
-                        <li><NavLink to="/update">Update Database</NavLink></li>
-                        <li><NavLink to="/upload">Upload CSV</NavLink></li>
                     </ul>
                     <div className="content">
                         <Routes>
                             <Route path="/" element={<Home />}/>
                             <Route path="/report" element={<ReportGen />}/>
                             <Route path="/backup" element={<Backup />}/>
-                            <Route path="/update" element={<Update />}/>
-                            <Route path="/upload" element={<Upload />}/>
                         </Routes>
                     </div>
                 </div>
